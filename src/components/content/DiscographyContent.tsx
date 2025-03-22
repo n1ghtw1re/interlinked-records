2c2a64debe76373978e037d4c1e43b9b89b25c40
Zero Coolness <zero.coolness@proton.me>
2025-03-22 18:26:42 +0700

    added int011

--- src/components/content/DiscographyContent.tsx
+++ src/components/content/DiscographyContent.tsx
@@ -64,6 +64,16 @@ const DiscographyContent: React.FC<ContentProps> = ({ setActiveContent }) => {
       tracks: ['CV Interface', 'Voltage Controlled', 'Patch Memory', 'Signal Path'],
       embedUrl: '<iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/album=711447107/size=large/bgcol=333333/linkcol=2ebd35/tracklist=false/artwork=small/transparent=true/" seamless><a href="https://interlinkedai.bandcamp.com/album/total-recall">Total Recall by Interlinked Records</a></iframe>',
       description: "Total Recall is a hard pulse-pumping peak time techno EP."
+    },
+    {
+      id: 'INT-011',
+      artist: 'TheDonStone',
+      title: 'Ms Ministry',
+      format: 'Digital EP',
+      year: '2024',
+      tracks: ['CV Interface', 'Voltage Controlled', 'Patch Memory', 'Signal Path'],
+      embedUrl: '<iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/track=1120552345/size=large/bgcol=333333/linkcol=2ebd35/tracklist=false/artwork=small/transparent=true/" seamless><a href="https://interlinkedai.bandcamp.com/track/ms-ministry">Ms. Ministry by Interlinked Records</a></iframe>',
+      description: "Ms. Minsitry fuses hard dark brooding IDM with Indide Dance vibes.'
     }
   ];
 
