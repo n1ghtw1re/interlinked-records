9f39a0e2e7410cd7f0e2c7d39d789c7067eb3c53
Zero Coolness <zero.coolness@proton.me>
2025-03-24 12:26:42 +0700

    adding new youtube mixes to dj mixes page

--- src/components/content/MixesContent.tsx
+++ src/components/content/MixesContent.tsx
@@ -8,7 +8,7 @@ const MixesContent: React.FC<ContentProps> = ({ setActiveContent }) => {
   const mixes = [
     {
       id: 'MIX-001',
-      title: 'SONA 003 - Dark Disco',
+      title: 'SONA 003 - Electro Goth',
       artist: 'TheDonStone',
       duration: '62:00',
       date: '2024-09',
@@ -28,24 +28,44 @@ const MixesContent: React.FC<ContentProps> = ({ setActiveContent }) => {
     },
     {
       id: 'MIX-003',
-      title: 'Subterranean Frequencies',
+      title: 'SONA 001 - Dark Disco',
       artist: 'Void Protocol',
       duration: '74:23',
       date: '2023-12-02',
-      tags: ['Dub Techno', 'Ambient', 'Industrial'],
-      embedUrl: '<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1224609325&color=%2346e62a&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>',
-      description: "Deep, resonant dub techno explorations from the underground. Void Protocol delivers a hypnotic journey through cavernous spaces and industrial landscapes."
+      tags: ['Dark Disco', 'Acid Techno', 'Hypnotic Techno'],
+      embedUrl: '<iframe width="560" height="315" src="https://www.youtube.com/embed/L-E95UOLK3Y?si=4DGaoLvaMInTotaf" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+      description: "In the inaugural episode of SONA, the enigmatic DJ known as TheDonStone takes us on an electrifying sonic journey through the realms of modern dark disco, acid techno, hypnotic techno, new wave, post-punk, and gothic soundscapes, all seamlessly woven together into a tapestry of underground rave bliss. Episode 001 of this groundbreaking mix series unveils a carefully curated selection of tracks from luminaries such as Depeche Mode, the evocative sounds of Jason Peters, the sultry beats of Dina Summer, and the edgy offerings of Local Suicide, among other noteworthy artists."
     },
     {
       id: 'MIX-004',
-      title: 'Subterranean Frequencies',
-      artist: 'Void Protocol',
-      duration: '74:23',
-      date: '2023-12-02',
-      tags: ['Dub Techno', 'Ambient', 'Industrial'],
-      embedUrl: '<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1224609325&color=%2346e62a&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>',
-      description: "Deep, resonant dub techno explorations from the underground. Void Protocol delivers a hypnotic journey through cavernous spaces and industrial landscapes."
+      title: 'LoFi For Hackers 003',
+      artist: 'imVexd',
+      duration: '60:14',
+      date: '2025-01',
+      tags: ['Dub Techno', 'Ambient', 'LoFi'],
+      embedUrl: '<iframe width="560" height="315" src="https://www.youtube.com/embed/PMvS5T7DkV0?si=beZxJY-W-jF60_nN" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+      description: "Immerse yourself in this atmospheric LoFi mix, blending dark ambient vibes with hypnotic dub techno beats. Designed for hackers, coders, and creators who thrive under pressure—even if Lovecraftian horrors and kaiju are tearing the city apart outside your window. This mix is your companion for deep focus, late-night grinding, or simply escaping into a world of ominous tranquility."
     },
+    {
+      id: 'MIX-005',
+      title: 'LoFi For Hackers 002',
+      artist: 'Axi0m',
+      duration: '61:07',
+      date: '2025-01',
+      tags: ['Chillout', 'Ambient', 'LoFi'],
+      embedUrl: '<iframe width="560" height="315" src="https://www.youtube.com/embed/DgCU0ymrk48?si=w4N2q_aIv-hCrWIX" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+      description: "Embark on a starship journey across the galaxy, coding and studying in the cozy ambiance of a celestial café. Immerse yourself in the soothing vibes of our synth lofi music for hackers and cyberpunks. This continuous stream of instrumental music is your perfect companion for deep focus, creative work, or unwinding after a long day. Drift through dreamy soundscapes, lush synth textures, mellow beats, and waves of nostalgic echoes that transport you to another world."
+    },
+    {
+      id: 'MIX-006',
+      title: 'LoFi For Hackers 001',
+      artist: 'Axi0m',
+      duration: '64:40',
+      date: '2025-01',
+      tags: ['Chillout', 'Ambient', 'LoFi'],
+      embedUrl: '<iframe width="560" height="315" src="https://www.youtube.com/embed/62Nc74qRP-8?si=fHHIHMHl0gJphE0J" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+      description: "Immerse yourself in the soothing vibes of our synth lofi music for hackers and cyberpunks. Tune in to this continuous stream of instrumental music, ideal for studying, working, or unwinding at home. Drift through dreamy soundscapes, rich synth textures, mellow beats, and waves of nostalgic echoes."
+    }
   ];
 
   const handleMixClick = (mix: any) => {
